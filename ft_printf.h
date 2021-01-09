c31720932525668c1ffbc728f51bb3081bb1449c
aziz-hammadi <ahammad@student.42.fr>
2021-01-09 11:18:21 +0100

    ft_printf.h

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: ahammad <ahammad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/06 21:36:31 by ahammad           #+#    #+#             */
-/*   Updated: 2021/01/09 11:17:37 by ahammad          ###   ########.fr       */
+/*   Updated: 2021/01/09 11:18:09 by ahammad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,28 +33,28 @@ typedef struct	s_format
 	void	(*format_func)(va_list *, t_options *);
 }				t_format;
 
-voidft_printf_str(va_list *my_list, t_options *op);
-voidft_printf_char(va_list *my_list, t_options *op);
-voidft_printf_nbr(va_list *my_list, t_options *op);
-voidft_printf_adress(va_list *my_list, t_options *op);
-voidft_printf_unsigned(va_list *my_list, t_options *op);
-voidft_printf_signed(va_list *my_list, t_options *op);
-voidft_printf_pourcent(va_list *my_list, t_options *op);
-voidft_printf_hexa(va_list *my_list, t_options *op);
-voidft_printf_hx(va_list *my_list, t_options *op);
-voidft_putnbr_u(unsigned int n, int *len);
-voidft_putchar(char c, int *len);
-voidft_putnbr(int n, int *len);
-size_tft_strlen(const char *s);
-voidft_putnbr_base(long unsigned int deci, char *str_base, int *l);
-int	ft_isdigit(int c);
-int	ft_atoi(const char *str);
-voidft_putnstr(char const *s, int n, int *len);
-int	ft_nbrlen(int num);
-int	ft_nbrlen_u(unsigned int nbr, int base);
-int	ft_nbrlen_lu(long unsigned int nbr, int base);
-int	ft_abs(int n);
-int	ft_printf(const char *format, ...);
-int	ft_check_op(va_list *ml, const char *src, int i, t_options *o);
+void			ft_printf_str(va_list *my_list, t_options *op);
+void			ft_printf_char(va_list *my_list, t_options *op);
+void			ft_printf_nbr(va_list *my_list, t_options *op);
+void			ft_printf_adress(va_list *my_list, t_options *op);
+void			ft_printf_unsigned(va_list *my_list, t_options *op);
+void			ft_printf_signed(va_list *my_list, t_options *op);
+void			ft_printf_pourcent(va_list *my_list, t_options *op);
+void			ft_printf_hexa(va_list *my_list, t_options *op);
+void			ft_printf_hx(va_list *my_list, t_options *op);
+void			ft_putnbr_u(unsigned int n, int *len);
+void			ft_putchar(char c, int *len);
+void			ft_putnbr(int n, int *len);
+size_t			ft_strlen(const char *s);
+void			ft_putnbr_base(long unsigned int deci, char *str_base, int *l);
+int				ft_isdigit(int c);
+int				ft_atoi(const char *str);
+void			ft_putnstr(char const *s, int n, int *len);
+int				ft_nbrlen(int num);
+int				ft_nbrlen_u(unsigned int nbr, int base);
+int				ft_nbrlen_lu(long unsigned int nbr, int base);
+int				ft_abs(int n);
+int				ft_printf(const char *format, ...);
+int				ft_check_op(va_list *ml, const char *src, int i, t_options *o);
 
 #endif
